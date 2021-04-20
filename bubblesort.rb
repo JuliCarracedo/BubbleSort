eac73a0baa0da4728caa097d40a745e5cb171679
Fondem Junior <fondemjunior@yahoo.com>
2021-04-20 17:49:08 +0100

    Add bubble_sort_by

--- bubblesort.rb
+++ bubblesort.rb
@@ -13,4 +13,21 @@ def bubble_sort(array)
   array
 end
 
+puts bubble_sort([2, 5, 9, 8, 7])
 
+def bubble_sort_by(array)
+  i = 0
+  j = 0
+  while j < array.length - 1
+    while i <= array.length - 2 - j
+      sorting = yield(array[i], array[i + 1])
+      array[i + 1], array[i] = array[i], array[i + 1] if sorting.negative?
+      i += 1
+    end
+    i = 0
+    j += 1
+  end
+  array
+end
+
+puts bubble_sort_by(%w[hi hello hey Here Howdy]) { |left, right| right.length - left.length }
